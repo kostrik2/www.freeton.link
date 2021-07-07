fa20e58e9eaa2299c217501965ffddad4ef795ba
kostrik2 <85219246+kostrik2@users.noreply.github.com>
2021-07-07 09:53:47 +0000

    Deploying to gh-pages from  @ 1d04627e4159c758bdc87e78c5fc07a51c85470b 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'master (d8d8fd3) (2021/07/07 09:53)',
+    VERSION: 'master (1d04627) (2021/07/07 09:53)',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
